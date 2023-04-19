61ff484ad567627fc82b7e0916abd73bd06af6d0
Henrique Vieira de Jesus <henriquerkn@gmail.com>
2023-04-19 20:10:40 -0300

    Adicionar arquivos de projeto.

---
+++ IdadeEmMesesEDias/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace IdadeEmMesesEDias
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonConvert = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAnos = new System.Windows.Forms.TextBox();
+            this.txtMeses = new System.Windows.Forms.TextBox();
+            this.txtDias = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonConvert
+            // 
+            this.buttonConvert.Location = new System.Drawing.Point(246, 93);
+            this.buttonConvert.Name = "buttonConvert";
+            this.buttonConvert.Size = new System.Drawing.Size(94, 45);
+            this.buttonConvert.TabIndex = 0;
+            this.buttonConvert.Text = "Clique Aqui";
+            this.buttonConvert.UseVisualStyleBackColor = true;
+            this.buttonConvert.Click += new System.EventHandler(this.buttonConvert_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Anos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Meses";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Dias";
+            // 
+            // txtAnos
+            // 
+            this.txtAnos.Location = new System.Drawing.Point(28, 44);
+            this.txtAnos.Name = "txtAnos";
+            this.txtAnos.Size = new System.Drawing.Size(100, 20);
+            this.txtAnos.TabIndex = 4;
+            // 
+            // txtMeses
+            // 
+            this.txtMeses.Location = new System.Drawing.Point(28, 106);
+            this.txtMeses.Name = "txtMeses";
+            this.txtMeses.Size = new System.Drawing.Size(100, 20);
+            this.txtMeses.TabIndex = 5;
+            // 
+            // txtDias
+            // 
+            this.txtDias.Location = new System.Drawing.Point(28, 165);
+            this.txtDias.Name = "txtDias";
+            this.txtDias.Size = new System.Drawing.Size(100, 20);
+            this.txtDias.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 239);
+            this.Controls.Add(this.txtDias);
+            this.Controls.Add(this.txtMeses);
+            this.Controls.Add(this.txtAnos);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonConvert);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonConvert;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAnos;
+        private System.Windows.Forms.TextBox txtMeses;
+        private System.Windows.Forms.TextBox txtDias;
+    }
+}
+
---
+++ IdadeEmMesesEDias/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IdadeEmMesesEDias
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonConvert_Click(object sender, EventArgs e)
+        {
+            int anos, totalMeses, totalDias;
+
+            if (int.TryParse(txtAnos.Text, out anos))
+            {
+                totalMeses = anos * 12;
+                totalDias = anos * 365;
+
+                txtMeses.Text = totalMeses.ToString();
+                txtDias.Text = totalDias.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Por favor, digite apenas números inteiros no campo de idade.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
